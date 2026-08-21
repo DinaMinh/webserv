02fd9c7992b27a7c18c428a06817754bc7b32c5f
DinaMinh <dminh.public@gmail.com>
2026-08-21 08:37:37 +0200

    [SOCKET]	I was able to bind the socket.

--- srcs/Socket.class.cpp
+++ srcs/Socket.class.cpp
@@ -16,13 +16,13 @@ Socket::Socket(const std::string &port, const std::string &pass)
 :	_host(LOC_HOST), _port(port), _pass(pass)
 {
 	(void)port, (void)pass;
-	if ((this->_sock = socket(DOMAIN, TYPE, PROTOCOL) < 0))
+	if ((this->_sock = socket(DOMAIN, TYPE, PROTOCOL)) == 0)
 		throw	std::runtime_error("error: Couldn't open the socket.");
 	std::cout << "sock = " << this->_sock << std::endl;
 	memset(&this->_addr, 0, sizeof(this->_addr));
 	this->_addr.sin_family = AF_INET;
 	this->_addr.sin_addr.s_addr = INADDR_ANY;
-	this->_addr.sin_family = htons(atoi(_port.c_str()));
+	this->_addr.sin_port = htons(atoi(_port.c_str()));
 	if (bind(_sock, (sockaddr *)&_addr, sizeof(_addr)) < 0)
 		throw	std::runtime_error("error: Couldn't bind the socket.");
 }
